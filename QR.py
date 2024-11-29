fc80d0458891a8e62641e0005ebb3dbe47c6444b
NavaGarcia <andres.jang@hotmail.com>
2024-11-28 23:40:47 -0700

    Limpieza

--- QR.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-import socket
-from datetime import datetime, timedelta,date
-host=socket.gethostname()
-Fecha= date.today()
-img = qrcode.make(f"Expedido por {host}, el dia {Fecha}")
-f = open("output.png", "wb")
-img.save(f)
-f.close()
